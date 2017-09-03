e6418ac648dceacd4d5cbf3e69b14ab7c4a557b2
Unknown <amano.marcos@gmail.com>
2017-09-02 21:15:07 -0300

    v1.10

---
+++ src/pkg/Comando.java
@@ -0,0 +1,24 @@
+package pkg;
+
+public class Comando {
+    private char tipo;   // 'E' para exibições, e 'A' para atibuições
+    private Object ref1;
+    private Object ref2;
+    public Comando(char _tipo, Object _ref1, Object _ref2) {
+          this.tipo = _tipo;
+          this.ref1 = _ref1;
+          this.ref2 = _ref2;
+    }
+    public char getTipo() {
+          return this.tipo;
+    }
+    public Object getRef1() {
+          return this.ref1;
+    }
+    public Object getRef2() {
+          return this.ref2;
+    }
+    public String toString() {
+          return this.tipo + "/" + this.ref1.toString() +  "/" + this.ref2.toString();
+    }
+}
